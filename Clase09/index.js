ba9c0c3fcdbc03ac67cfc921e3b16de5f2034542
Pablo Pulisich <pablopuli94@gmail.com>
2021-08-24 16:47:00 -0300

    Entregable 09 NO COMPLEMENTARIO, entregado

--- Clase09/index.js
+++ Clase09/index.js
@@ -6,7 +6,7 @@ let boton = document.getElementById("miBoton");
 
 boton.addEventListener ("click", interactuar)
 
-function interactuar{
+function interactuar(){
     let nombre = prompt("Ingrese su nombre");
     let fecha = new Date();
     alert("Hola "+ nombre + " hoy es " + fecha);
