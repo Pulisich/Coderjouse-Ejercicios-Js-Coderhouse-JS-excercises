97b78a0a81683bcfa61cc9ca86aba58142c96cc8
Pablo Pulisich <pablopuli94@gmail.com>
2021-08-31 17:31:33 -0300

    Clase 10 fue workshop no vimos nada práctico

--- Clase09/clasePulisich09.js
+++ Clase09/clasePulisich09.js
@@ -1,40 +1,40 @@
-let nombre=prompt("¿Cuál es tu nombre?")
-let apellido=prompt("¿Cuál es tu nombre de familia?")
+// let nombre=prompt("¿Cuál es tu nombre?")
+// let apellido=prompt("¿Cuál es tu nombre de familia?")
 
 
-console.log(nombre + " "+ apellido);
-alert("Bienvenido " + nombre +" "+ apellido);
+// console.log(nombre + " "+ apellido);
+// alert("Bienvenido " + nombre +" "+ apellido);
 
- function nacimiento(anio, edad) {
-     let fecha= anio - edad
-     return fecha  
-} 
+//  function nacimiento(anio, edad) {
+//      let fecha= anio - edad
+//      return fecha  
+// } 
 
- let edadActual= parseInt(prompt("¿Cuántos años tenés?"));
- let anioActual= 2021;
- let anioNacimiento= nacimiento(anioActual, edadActual)
- console.log(anioNacimiento)
+//  let edadActual= parseInt(prompt("¿Cuántos años tenés?"));
+//  let anioActual= 2021;
+//  let anioNacimiento= nacimiento(anioActual, edadActual)
+//  console.log(anioNacimiento)
 
- if (anioNacimiento > 2003){
-    alert("No vendemos a menores de edad, volvé con un adulto.")
-}
+//  if (anioNacimiento > 2003){
+//     alert("No vendemos a menores de edad, volvé con un adulto.")
+// }
 
-let telefono= prompt("Ingrese un número de teléfono")
-console.log(telefono)
-
-
-
- sessionStorage.setItem("Nombre", nombre);
- sessionStorage.setItem("Apellido", apellido);
- sessionStorage.setItem("Telefono", telefono);
+// let telefono= prompt("Ingrese un número de teléfono")
+// console.log(telefono)
 
 
 
-let presentacion = document.getElementById("nombreDom")
-function presentar(){
-    presentacion.innerText = nombre + " " + apellido
-}
-presentar();
+//  sessionStorage.setItem("Nombre", nombre);
+//  sessionStorage.setItem("Apellido", apellido);
+//  sessionStorage.setItem("Telefono", telefono);
+
+
+
+// let presentacion = document.getElementById("nombreDom")
+// function presentar(){
+//     presentacion.innerText = nombre + " " + apellido
+// }
+// presentar();
  
 
 
@@ -98,30 +98,18 @@ console.log(productos.sort(compare));
 
 //BOTONES CARRITO
 
+let limpiarCarro= document.getElementsByClassName("btn-danger")
+for (let i=0; i < limpiarCarro.length; i++){
+    let boton= limpiarCarro[i];
+    boton.addEventListener("click", function(evento){
+        botonClick= evento.target
+        botonClick.parentElement
+    })
+}
+
 agregarCarro= document.getElementById("botonCompra1")
-agregarCarro.onclick = () => {alert("¿Querés agregar este producto al carrito?");
-console.log("Producto 1 agregado al carro")}; //Después ver bien cómo asociar productos a botones.
+agregarCarro.onclick = () => {
+    document.getElementById("itemsCarro")
+console.log("Producto 1 agregado al carro")
+}; //Después ver bien cómo asociar productos a botones.
 
-agregarCarro2= document.getElementById("botonCompra2")
-agregarCarro2.onclick = () => {alert("¿Querés agregar este producto al carrito?");
-console.log("Producto 2 agregado al carro")}; //Después ver bien cómo acumular más de un producto.
-
-agregarCarro3= document.getElementById("botonCompra3")
-agregarCarro3.onclick = () => {alert("¿Querés agregar este producto al carrito?");
-console.log("Producto 3 agregado al carro")}; 
-
-agregarCarro4= document.getElementById("botonCompra4")
-agregarCarro4.onclick = () => {alert("¿Querés agregar este producto al carrito?");
-console.log("Producto 4 agregado al carro")}; 
-
-agregarCarro5= document.getElementById("botonCompra5")
-agregarCarro5.onclick = () => {alert("¿Querés agregar este producto al carrito?");
-console.log("Producto 5 agregado al carro")}; 
-
-agregarCarro6= document.getElementById("botonCompra6")
-agregarCarro6.onclick = () => {alert("¿Querés agregar este producto al carrito?");
-console.log("Producto 6 agregado al carro")}; 
-
-let carro= document.getElementById("carrito");
-carro.onclick= () => alert("El carro está vacío");
-carro.onmouseover= () => console.log("El carro es este aunque tiene forma de cesta")
