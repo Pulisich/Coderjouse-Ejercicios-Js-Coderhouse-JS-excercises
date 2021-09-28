8fa1979651a1c4eaf5b9feff4f02b4488cc05520
Pablo Pulisich <pablopuli94@gmail.com>
2021-09-28 18:19:30 -0300

    Preparado proyecto para entrega

--- TerceraEntrega/scripts.js
+++ TerceraEntrega/scripts.js
@@ -40,7 +40,17 @@ $(document).ready(function(){
     $("#limpiarCarro").on("click", function(){
         $("#dentro").children().remove();
 
-        localStorage.removeItem("En Carro");
+        localStorage.clear();
+
+
+        localStorage.setItem("Productos disponibles", JSON.stringify(productos));
+
+        //CHAU TODOS LOS ITEMS, CUANDO AGREGÁS OTRO ITEM AL CARRO YA VACÍO SE REINICIA TODO
+
+         localStorage.getItem("En Carro", listaCarrito.splice([0]))
+
+        
+        
         
         Swal.fire({
             
@@ -49,6 +59,8 @@ $(document).ready(function(){
             showConfirmButton: false,
             timer: 1000
         })
+
+        console.log("El carro está vacío")
     })
 
 
@@ -75,83 +87,101 @@ $(document).ready(function(){
 
 const renderProductos =()=>{
     for (const producto of productos) {
-     $(".ulistaProd").append(`<li class="list-group-item"><h3> ${producto.tipo}</h3></li>
-     <li class="list-group-item"><h2>  Relleno: ${producto.relleno}</h2></li>
+     $(".ulistaProd").append(`<li class="list-group-item border-0"><h3> ${producto.tipo}</h3></li>
+     <li class="list-group-item border-0"><h2>  Relleno: ${producto.relleno}</h2></li>
      <div> <img src= ${producto.foto} width: "55" height: "auto" class = "imgProd"></div>
-     <li class="list-group-item"><p> Cantidad por caja: ${producto.caja}</p></li>
-     <li class="list-group-item"><b> Precio: $${producto.precio}</b></li>
+     <li class="list-group-item border-0"><p> Cantidad por caja: ${producto.caja}</p></li>
+     <li class="list-group-item border-0"><b> Precio: $${producto.precio}</b></li>
      <button type="button" id="boton${producto.id}"> Agregar al carro </button> `); //Genera un botón por cada producto con su propio ID
     
     
-     //BOTONES PARA AGREGAR AL CARRO POR CADA ITEM
+        //BOTONES PARA AGREGAR AL CARRO POR CADA ITEM
 
-     $(`#boton${producto.id}`).on("click",function(){
+        $(`#boton${producto.id}`).on("click",function(){
+            
+            //REMOVEMOS EL TOTAL DEL CARRO CADA VEZ QUE AGREGAMOS UN PRODUCTO Y SE ACTUALIZA DESPUÉS
 
-        //ME CREA UNA MINI CARD DEL PRODUCTO EN EL CARRO
+            let enCarro= $(".total").children().remove("#totalCarro");
 
-        console.log(`${producto.tipo}  relleno de  ${producto.relleno} agregado al carro`)
-        $("#dentro").append(`<div id="itemCarro"><p> ${producto.tipo}</p>
-        <p>${producto.relleno}</p>
-        <p>${producto.caja}</p
-        <p>$${producto.precio}</p>
-        <button class="btn-danger quitarItem"> Quitar </button></div> 
-        `)
+            //ME CREA UNA MINI CARD DEL PRODUCTO EN EL CARRO
 
-        //PARA SACAR EL ITEM DEL CARRO DE A UNO
-     
-        let quitarCarro = document.getElementsByClassName("quitarItem")
+            console.log(`${producto.tipo}  relleno de  ${producto.relleno} agregado al carro`)
+            $("#dentro").append(`<div id="itemCarro"><p> ${producto.tipo}</p>
+            <p>${producto.relleno}</p>
+            <p>${producto.caja}</p
+            <p>$${producto.precio}</p>
+            <button class="btn-danger quitarItem"> Quitar </button></div> 
+            `)
+
+
+
+            //AGREGA PRODUCTOS AL ARRAY CARRITO USANDO PUSH, EL CONSTRUCTOR ES PASTA, LOS DATOS VIENEN DE LOS PRODUCTOS
            
-        for( var i = 0; i < quitarCarro.length; i++){
-            let boton = quitarCarro[i];
-            boton.addEventListener("click", function(event){
-                let botonClick= event.target;
-                botonClick.parentElement.remove(function(){
-                    botonClick.parentElement.fadeOut(3000)
-                });
-                console.log(` ${producto.tipo}  relleno de  ${producto.relleno} eliminado del carro.`)
-                listaCarrito.slice(listaCarrito[0])
-                
-            })
+            listaCarrito.push(new Pasta (producto.tipo, producto.relleno, producto.caja, producto.precio));
+                    
+            console.log(listaCarrito);
             
-        }
 
+            // CARRITO ALMACENADO EN LOCAL STORAGE LOS PRODUCTOS TIENEN SU PROPIO ID
+        
+            localStorage.setItem("En Carro",JSON.stringify(listaCarrito))
+            
+            //CALCULO EL TOTAL DEL CARRO USANDO REDUCE PARA AGARRAR EL PRECIO
 
-
-        listaCarrito.push(new Pasta (producto.tipo, producto.relleno, producto.caja, producto.precio));
-        
-        console.log(listaCarrito);
-        
-
-        // CARRITO EN LOCAL STORAGE LOS PRODUCTOS TIENEN SU PROPIO ID
-        
-        localStorage.setItem("En Carro",JSON.stringify(listaCarrito))
-        
+           const total = listaCarrito.map( producto => producto.precio).reduce((prev, next)=> prev + next);
+          
+           console.log( "Su total es de: $" + total);
          
-         if(localStorage.getItem("En Carro") != null){
-         let elCarro= JSON.parse(localStorage.getItem("En Carro"))
-   
-         let total= 0
+            //ACTUALIZAMOS EL TOTAL (NOTA: EL TOTAL NO SE BORRA PERO SE MODIFICA CON CADA ITEM QUE SE AGREGA CUANDO ELIMINAMOS DE A UNO)
 
-         for( const producto of listaCarrito){
-             total = total + producto.precio;
-             console.log( "Su total es de:" + total)
-         }
-        //  let cantidadCarro= 0
+           let totalCarro = $(".total").prepend(`<p id="totalCarro">TOTAL: $${total}</p>`)
+
+
+
+
+             //PARA SACAR EL ITEM DEL CARRO DE A UNO
+     
+             //EN EL HTML BORRA DE A UNO
+            let quitarCarro = document.getElementsByClassName("quitarItem")
             
-         for (const producto of elCarro){
-        //  cantidadCarro = cantidadCarro + 1
-         total= total + producto.precio
-       
-     }
-         //  console.log(cantidadCarro)
-        //   console.log(total)
-   }
+            for( var i = 0; i < quitarCarro.length; i++){
+                let boton = quitarCarro[i];
+                boton.addEventListener("click", function(event){
+                    let botonClick= event.target;
+                    botonClick.parentElement.remove(function(){
+                        botonClick.parentElement.fadeOut(3000)
+                    });
+                    console.log(` ${producto.tipo}  relleno de  ${producto.relleno} eliminado del carro.`)
 
-        //¿Cómo hago para que me eliminar los productos por separado en el storage?
-    
-        
-    })
-    
+
+                    //BORRANDO DEL LOCAL STORAGE
+
+                    let enLocal = localStorage.getItem("En Carro",listaCarrito);
+                    
+                    let id = 0
+
+                    listaCarrito.filter(item => item != id);
+
+                    
+
+
+
+                    localStorage.setItem("En Carro",JSON.stringify (listaCarrito))
+                   
+                })
+            }
+
+
+
+                     
+            $(".total").children().remove("#totalCarro");
+
+            $(".total").prepend(`<p id="totalCarro">TOTAL: $${total}</p>`)
+                
+            
+                
+        })
+            
     }
 }
 
@@ -167,9 +197,9 @@ function compare (a, b){
 
 
 
-//Array con todos los productos 
+//ARRAY DE PRODUCTOS DEL CARRITO
 
-let listaCarrito= [];
+const listaCarrito= [];
 
 class Pasta {
     constructor(tipo, relleno, caja, precio){
@@ -177,12 +207,12 @@ class Pasta {
         this.relleno= relleno;
         this.caja= caja;
         this.precio= precio;
-        this.cantidad= 1;
+        
     }
+//ACÁ A LO MEJOR PUEDE ESTAR LA RESPUESTA PARA CALCULAR EL TOTAL
 };
 
 
 console.log(productos.sort(compare));
 
- //TOTAL CARRO
 
